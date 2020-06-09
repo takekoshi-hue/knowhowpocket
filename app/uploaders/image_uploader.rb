1e204103fd5bb502e5bde51deb8ec068074d7ba1
masato.takekoshi <koshitake1@gmail.com>
2020-06-09 12:24:45 +0000

    compe

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,13 +18,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   def default_url(*args)
-   "default.jpg"
+   "defa.png"
+   
+  
+   
   end
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+   #process scale: [100, 100]
   #
   # def scale(width, height)
   #   # do something
